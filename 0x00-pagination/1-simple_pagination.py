a61900d331675fd87a54d5737171766001f7b1d1
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-10-29 12:59:20 +0000

    paginating a csv file

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -33,6 +33,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''Retriving a page of data
+        '''
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
